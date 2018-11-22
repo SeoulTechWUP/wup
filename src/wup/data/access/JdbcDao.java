833126563d5a7b82fe24beebd4d4e985590abac9
Dalgona <dalgona@hontou.moe>
2018-11-22 17:56:30 +0900

    DaoResult, JdbcDao 클래스 추가, UserDao 인터페이스 추가

---
+++ src/wup/data/access/JdbcDao.java
@@ -0,0 +1,34 @@
+package wup.data.access;
+
+import java.sql.*;
+import javax.naming.*;
+import javax.sql.*;
+
+/**
+ * JDBC를 사용하는 DAO의 상위 클래스입니다.
+ *
+ * @author Eunbin Jeong
+ */
+public abstract class JdbcDao {
+    /**
+     * <code>name</code>으로 지정한 JDBC 연결을 가져옵니다.
+     * @param name 리소스 이름
+     * @return <code>name</code>이 존재하는 경우,
+     * 해당하는 {@link java.sql.Connection} 객체를 반환하고,
+     * 없으면 <code>null</code>을 반환합니다.
+     * @throws SQLException
+     */
+    protected final Connection getConnection(String name) throws SQLException {
+	try {
+            Context initCtx = new InitialContext();
+            Context ctx = (Context)initCtx.lookup("java:/comp/env");
+            DataSource ds = (DataSource)ctx.lookup(name);
+
+            return ds.getConnection();
+	} catch (NamingException e) {
+	    e.printStackTrace();
+
+            return null;
+        }
+    }
+}
---
+++ src/wup/data/access/UserDao.java
@@ -0,0 +1,38 @@
+package wup.data.access;
+
+import wup.data.User;
+
+/**
+ * 사용자 정보 DAO에 대한 인터페이스입니다.
+ *
+ * @author Eunbin Jeong
+ */
+public interface UserDao {
+    /**
+     * 주어진 id에 해당하는 사용자를 가져옵니다.
+     * @param id 사용자 일련번호
+     */
+    public DaoResult<User> getUser(int id);
+
+    /**
+     * 새로운 사용자를 생성합니다.
+     * @param user 사용자 생성 뷰에서 입력된 정보
+     * @param auth 암호(평문). 데이터베이스에 해시되어 저장됩니다.
+     */
+    public DaoResult<User> createUser(User user, String auth);
+
+    /**
+     * 주어진 id에 해당하는 사용자의 정보를 변경합니다.
+     * @param id 변경할 사용자의 일련번호
+     * @param user 변경할 정보가 들어 있는 {@link User} 객체
+     * @param oldAuth 암호를 변경하는 경우에는 기존 암호, 그렇지 않으면 <code>null</code> 또는 빈 문자열
+     * @param newAuth 암호를 변경하는 경우에는 새로운 암호, 그렇지 않으면 <code>null</code> 또는 빈 문자열
+     */
+    public DaoResult<User> updateUser(int id, User user, String oldAuth, String newAuth);
+
+    /**
+     * 주어진 id에 해당하는 사용자를 삭제합니다.
+     * @param id 삭제할 사용자의 일련번호
+     */
+    public DaoResult<Boolean> deleteUser(int id);
+}

bc18da4058bc3f7ef923152832e82d5decd65d33
Eunbin Jeong <dmsqls7353@gmail.com>
2018-11-29 23:00:32 +0900

    UserAuthServlet: POST /api/user/auth 구현

--- wup-app/src/wup/servlet/api/UserAuthServlet.java
+++ wup-app/src/wup/servlet/api/UserAuthServlet.java
@@ -2,12 +2,21 @@ package wup.servlet.api;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import wup.api.Error;
+import wup.api.GsonHolder;
+import wup.data.User;
+import wup.data.access.DaoFactory;
+import wup.data.access.DaoResult;
+import wup.data.access.MariaDbDaoFactory;
+import wup.data.access.UserDao;
 
 /**
  * Servlet implementation class UserAuthServlet
@@ -23,11 +32,47 @@ public class UserAuthServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/plain; charset=UTF-8");
+        response.setContentType("text/plain; charset=utf-8");
 
+        MariaDbDaoFactory daoFactory = new DaoFactory();
+        UserDao userDao = (UserDao) daoFactory.getDao(User.class);
+
+        String email = request.getParameter("email");
+        String auth = request.getParameter("auth");
+
+        if (email == null) {
+            email = "";
+        }
+
+        if (auth == null) {
+            auth = "";
+        }
+
+        DaoResult<User> authResult = userDao.authenticate(email, auth);
         PrintWriter out = response.getWriter();
 
-        out.println("로그인 처리");
+        if (authResult.didSucceed()) {
+            User authenticatedUser = authResult.getData();
+
+            if (authenticatedUser == null) {
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                out.println(new Error(Error.E_LOGINFAIL).toJson());
+            } else {
+                request.getSession().setAttribute("authenticatedUser", authenticatedUser);
+                response.setStatus(HttpServletResponse.SC_OK);
+                out.println(GsonHolder.getGson().toJson(authenticatedUser));
+            }
+        } else {
+            Exception e = authResult.getException();
+
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+
+            if (e instanceof SQLException) {
+                out.println(new Error(Error.E_DBERROR).toJson());
+            } else {
+                out.println(new Error(-1));
+            }
+        }
     }
 
     /**

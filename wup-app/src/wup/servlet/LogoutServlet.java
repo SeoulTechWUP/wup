b1c13e8b53ca92f7570e58893f5a72fbd64e8e40
Eunbin Jeong <dmsqls7353@gmail.com>
2018-11-30 15:41:42 +0900

    로그아웃 Servlet 구현

---
+++ wup-app/src/wup/servlet/LogoutServlet.java
@@ -0,0 +1,29 @@
+package wup.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/processLogout")
+public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getSession().invalidate();
+        response.sendRedirect("/");
+    }
+
+}

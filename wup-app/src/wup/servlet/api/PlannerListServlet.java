36d43ba831b5ceb661972cdcb668f868aac28fb4
Eunbin Jeong <dmsqls7353@gmail.com>
2018-12-02 19:39:33 +0900

    PlannerListServlet 구현

--- wup-app/src/wup/servlet/api/PlannerListServlet.java
+++ wup-app/src/wup/servlet/api/PlannerListServlet.java
@@ -1,12 +1,25 @@
 package wup.servlet.api;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import wup.api.GsonHolder;
+import wup.data.Planner;
+import wup.data.User;
+import wup.data.access.DaoFactory;
+import wup.data.access.DaoResult;
+import wup.data.access.MariaDbDaoFactory;
+import wup.data.access.PlannerDao;
+import wup.servlet.ServletHelper;
 
 /**
  * Servlet implementation class PlannerListServlet
@@ -22,8 +35,27 @@ public class PlannerListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        response.setContentType("application/json; charset=utf-8");
+
+        User authenticatedUser = ServletHelper.checkAuth(request, response);
+
+        if (authenticatedUser == null) {
+            return;
+        }
+
+        PrintWriter out = response.getWriter();
+        Gson gson = GsonHolder.getGson();
+        MariaDbDaoFactory daoFactory = new DaoFactory();
+        PlannerDao plannerDao = (PlannerDao) daoFactory.getDao(Planner.class);
+
+        DaoResult<List<Planner>> getPlannersResult = plannerDao.getPlanners(authenticatedUser);
+
+        if (!getPlannersResult.didSucceed()) {
+            ServletHelper.onDaoError(response, getPlannersResult);
+        } else {
+            response.setStatus(HttpServletResponse.SC_OK);
+            out.println(gson.toJson(getPlannersResult.getData()));
+        }
     }
 
 }

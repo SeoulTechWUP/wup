36baef9b795e428de1b229fd5e31d75a587acb1c
Eunbin Jeong <dmsqls7353@gmail.com>
2018-12-17 07:30:58 +0900

    NewScheduleServlet, ScheduleServlet 추가

---
+++ wup-app/src/wup/servlet/NewScheduleServlet.java
@@ -0,0 +1,64 @@
+package wup.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import wup.data.User;
+
+/**
+ * Servlet implementation class NewScheduleServlet
+ */
+@WebServlet("/newSchedule")
+public class NewScheduleServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        User authenticatedUser = (User) request.getSession().getAttribute("authenticatedUser");
+
+        if (authenticatedUser == null || authenticatedUser.getEmail() == null) {
+            response.sendRedirect(request.getContextPath() + "/login.jsp");
+
+            return;
+        }
+
+        String plannerId = request.getParameter("plannerId");
+
+        if (plannerId == null || plannerId.isEmpty()) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+
+            return;
+        }
+
+        request.setAttribute("mode", "new");
+        request.setAttribute("plannerId", plannerId);
+        request.getRequestDispatcher("/scheduleView.jsp").forward(request, response);
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        User authenticatedUser = (User) request.getSession().getAttribute("authenticatedUser");
+
+        if (authenticatedUser == null || authenticatedUser.getEmail() == null) {
+            response.sendRedirect(request.getContextPath() + "/login.jsp");
+
+            return;
+        }
+    }
+
+}
---
+++ wup-app/src/wup/servlet/ScheduleServlet.java
@@ -0,0 +1,47 @@
+package wup.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ScheduleServlet
+ */
+@WebServlet("/schedule/*")
+public class ScheduleServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
+
+    /**
+     * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+     */
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+    }
+
+}

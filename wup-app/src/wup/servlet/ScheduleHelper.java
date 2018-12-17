8f6039e634e8a2bf85e7428af8145edefddb8871
Eunbin Jeong <dmsqls7353@gmail.com>
2018-12-17 22:52:35 +0900

    일정의 접근 권한을 확인하고 가져오는 기능을 별도의 클래스로 분리

---
+++ wup-app/src/wup/servlet/ScheduleHelper.java
@@ -0,0 +1,87 @@
+package wup.servlet;
+
+import javax.servlet.http.HttpServletResponse;
+
+import wup.data.Group;
+import wup.data.ItemOwner;
+import wup.data.Planner;
+import wup.data.Schedule;
+import wup.data.User;
+import wup.data.access.DaoFactory;
+import wup.data.access.DaoResult;
+import wup.data.access.GroupDao;
+import wup.data.access.MariaDbDaoFactory;
+import wup.data.access.ScheduleDao;
+
+/**
+ * 서블릿에서 일정 관련 정보를 가져오는 데 도움을 주는 메서드를 제공하는 클래스입니다.
+ *
+ * @author Eunbin Jeong
+ */
+class ScheduleHelper {
+    public static class RequireResult {
+        public final Schedule schedule;
+        public final int errorCode;
+        public final Exception exception;
+
+        private RequireResult(Schedule schedule, int errorCode, Exception exception) {
+            this.schedule = schedule;
+            this.errorCode = errorCode;
+            this.exception = exception;
+        }
+
+        public static RequireResult succeed(Schedule schedule) {
+            return new RequireResult(schedule, -1, null);
+        }
+
+        public static RequireResult fail(int errorCode) {
+            return new RequireResult(null, errorCode, null);
+        }
+
+        public static RequireResult fail(Exception exception) {
+            return new RequireResult(null, -1, exception);
+        }
+    }
+
+    public static RequireResult requireSchedule(int scheduleId, User user) {
+        MariaDbDaoFactory daoFactory = new DaoFactory();
+        ScheduleDao scheduleDao = (ScheduleDao) daoFactory.getDao(Schedule.class);
+        DaoResult<Schedule> getScheduleResult = scheduleDao.getSchedule(scheduleId);
+
+        if (!getScheduleResult.didSucceed()) {
+            return RequireResult.fail(getScheduleResult.getException());
+        }
+
+        Schedule schedule = getScheduleResult.getData();
+
+        if (schedule == null) {
+            return RequireResult.fail(HttpServletResponse.SC_NOT_FOUND);
+        }
+
+        DaoResult<Boolean> getAccessResult = hasAccess(schedule, user);
+
+        if (!getAccessResult.didSucceed()) {
+            return RequireResult.fail(getAccessResult.getException());
+        } else if (!getAccessResult.getData()) {
+            return RequireResult.fail(HttpServletResponse.SC_FORBIDDEN);
+        } else {
+            return RequireResult.succeed(schedule);
+        }
+    }
+
+    private static DaoResult<Boolean> hasAccess(Schedule schedule, User user) {
+        Planner planner = schedule.getPlanner();
+
+        if (planner.getType() == ItemOwner.Type.USER) {
+            // 플래너 소유자 일치 여부 확인
+            return DaoResult.succeed(DaoResult.Action.READ, planner.getOwner().getId() == user.getId());
+        } else {
+            // 플래너를 소유하고 있는 그룹에 소속되어 있는지 여부 확인
+            MariaDbDaoFactory daoFactory = new DaoFactory();
+            GroupDao groupDao = (GroupDao) daoFactory.getDao(Group.class);
+            Group owningGroup = (Group) planner.getOwner();
+
+            return groupDao.isMember(owningGroup, user);
+        }
+    }
+}
--- wup-app/src/wup/servlet/ScheduleServlet.java
+++ wup-app/src/wup/servlet/ScheduleServlet.java
@@ -10,16 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import wup.data.Group;
-import wup.data.ItemOwner;
-import wup.data.Planner;
-import wup.data.Schedule;
 import wup.data.User;
-import wup.data.access.DaoFactory;
-import wup.data.access.DaoResult;
-import wup.data.access.GroupDao;
-import wup.data.access.MariaDbDaoFactory;
-import wup.data.access.ScheduleDao;
 
 /**
  * Servlet implementation class ScheduleServlet
@@ -73,7 +64,7 @@ public class ScheduleServlet extends HttpServlet {
             return;
         }
 
-        RequireScheduleResult rs = requireSchedule(id, authenticatedUser);
+        ScheduleHelper.RequireResult rs = ScheduleHelper.requireSchedule(id, authenticatedUser);
 
         if (rs.exception != null) {
             throw new ServletException(rs.exception);
@@ -118,72 +109,6 @@ public class ScheduleServlet extends HttpServlet {
     protected void doDelete(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         // TODO Auto-generated method stub
-    }
-
-    private static class RequireScheduleResult {
-        public final Schedule schedule;
-        public final int errorCode;
-        public final Exception exception;
-
-        private RequireScheduleResult(Schedule schedule, int errorCode, Exception exception) {
-            this.schedule = schedule;
-            this.errorCode = errorCode;
-            this.exception = exception;
-        }
-
-        public static RequireScheduleResult succeed(Schedule schedule) {
-            return new RequireScheduleResult(schedule, -1, null);
-        }
-
-        public static RequireScheduleResult fail(int errorCode) {
-            return new RequireScheduleResult(null, errorCode, null);
-        }
-
-        public static RequireScheduleResult fail(Exception exception) {
-            return new RequireScheduleResult(null, -1, exception);
-        }
-    }
-
-    private RequireScheduleResult requireSchedule(int scheduleId, User user) {
-        MariaDbDaoFactory daoFactory = new DaoFactory();
-        ScheduleDao scheduleDao = (ScheduleDao) daoFactory.getDao(Schedule.class);
-        DaoResult<Schedule> getScheduleResult = scheduleDao.getSchedule(scheduleId);
-
-        if (!getScheduleResult.didSucceed()) {
-            return RequireScheduleResult.fail(getScheduleResult.getException());
-        }
-
-        Schedule schedule = getScheduleResult.getData();
-
-        if (schedule == null) {
-            return RequireScheduleResult.fail(HttpServletResponse.SC_NOT_FOUND);
-        }
-
-        DaoResult<Boolean> getAccessResult = hasAccess(schedule, user);
-
-        if (!getAccessResult.didSucceed()) {
-            return RequireScheduleResult.fail(getAccessResult.getException());
-        } else if (!getAccessResult.getData()) {
-            return RequireScheduleResult.fail(HttpServletResponse.SC_FORBIDDEN);
-        } else {
-            return RequireScheduleResult.succeed(schedule);
-        }
-    }
-
-    private DaoResult<Boolean> hasAccess(Schedule schedule, User user) {
-        Planner planner = schedule.getPlanner();
-
-        if (planner.getType() == ItemOwner.Type.USER) {
-            // 플래너 소유자 일치 여부 확인
-            return DaoResult.succeed(DaoResult.Action.READ, planner.getOwner().getId() == user.getId());
-        } else {
-            // 플래너를 소유하고 있는 그룹에 소속되어 있는지 여부 확인
-            MariaDbDaoFactory daoFactory = new DaoFactory();
-            GroupDao groupDao = (GroupDao) daoFactory.getDao(Group.class);
-            Group owningGroup = (Group) planner.getOwner();
-
-            return groupDao.isMember(owningGroup, user);
-        }
     }
 
 }

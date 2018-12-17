18b12fa47a46dee3a84ffc61613eddad5f194439
Eunbin Jeong <dmsqls7353@gmail.com>
2018-12-17 20:59:45 +0900

    자동 코드 포매팅 실행

--- wup-app/src/wup/servlet/MediaServlet.java
+++ wup-app/src/wup/servlet/MediaServlet.java
@@ -1,17 +1,11 @@
 package wup.servlet;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,115 +14,115 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-
 import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 import wup.data.Media;
 import wup.data.Media.Type;
 import wup.data.Post;
-import wup.data.access.MediaDao;
 import wup.data.access.DaoFactory;
 import wup.data.access.DaoResult;
 import wup.data.access.MariaDbDaoFactory;
-
+import wup.data.access.MediaDao;
 
 @WebServlet("/media/*")
 public class MediaServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private static final Pattern MediaURLPattern; //URL 검사 표현식   
-	private static final Pattern MediaPathURLPattern; //URL 검사 표현식 
-	private static final int FileMaxSize = 1024*1024*50;
+    private static final long serialVersionUID = 1L;
+    private static final Pattern MediaURLPattern; // URL 검사 표현식
+    private static final Pattern MediaPathURLPattern; // URL 검사 표현식
+    private static final int FileMaxSize = 1024 * 1024 * 50;
 
     static {
         MediaURLPattern = Pattern.compile("^/(?<post>[1-9][0-9]*)$");
         MediaPathURLPattern = Pattern.compile("^/(?<post>[1-9][0-9]*)/(?<type>image|video)$");
     }
-	
-    //URL 검사 메소드
+
+    // URL 검사 메소드
     private int ValidatePath(String pathString) {
         int post = -1;
-        
+
         Matcher mat = MediaURLPattern.matcher(pathString);
-        
-        if(mat.matches()) {
+
+        if (mat.matches()) {
             post = Integer.parseInt(mat.group("post"));
         }
-        
+
         return post;
     }
-    
-    //json 변환 메소드
+
+    // json 변환 메소드
     private String makeJSON(Object obj, String result) {
         Gson gson = new Gson();
         JsonObject jobj = new JsonObject();
-        
+
         String objson = gson.toJson(obj);
         String json;
-        
+
         jobj.addProperty("data", objson);
         jobj.addProperty("result", result);
-        
+
         json = gson.toJson(jobj);
-        
+
         return json;
     }
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-	        throws ServletException, IOException {
-	    
-	    response.setContentType("application/json");
-	    
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("application/json");
+
         MariaDbDaoFactory daoFactory = new DaoFactory();
         MediaDao MediaDao = (MediaDao) daoFactory.getDao(Media.class);
-        
+
         DaoResult<List<Media>> getMedia;
-        
+
         Post post = new Post();
-        
+
         int postNum = ValidatePath(ServletHelper.trimString(request.getPathInfo()));
-            
+
         if (postNum < 0) {
             response.getWriter().write(makeJSON(null, "pathfail"));
             return;
-        } 
-        
+        }
+
         post.setId(postNum);
         getMedia = MediaDao.getMedia(post);
-        
-        if(getMedia.didSucceed()) {
+
+        if (getMedia.didSucceed()) {
             response.getWriter().write(makeJSON(getMedia.getData(), "success"));
         } else {
             response.getWriter().write(makeJSON(getMedia.getException().getMessage(), "dbfail"));
             return;
         }
-	}
+    }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
-	        throws ServletException, IOException {
-	    
-	    response.setContentType("application/json");
-	    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("application/json");
+
         MariaDbDaoFactory daoFactory = new DaoFactory();
         MediaDao MediaDao = (MediaDao) daoFactory.getDao(Media.class);
-        
+
         DaoResult<Media> createMedia;
-        
+
         Media tempMedia = new Media();
         Post tempPost = new Post();
-	    
+
         File[] fileList;
-        
-	    MultipartRequest mul;
-	    String realPath = this.getServletContext().getRealPath("/res/");
-	    String path = "";
-	    
+
+        MultipartRequest mul;
+        String realPath = this.getServletContext().getRealPath("/res/");
+        String path = "";
+
         String type = null;
         int post = -1;
-        
+
         Matcher mat = MediaPathURLPattern.matcher(ServletHelper.trimString(request.getPathInfo()));
-        
-        if(mat.matches()) {
+
+        if (mat.matches()) {
             type = mat.group("type");
             post = Integer.parseInt(mat.group("post"));
             if (mat.group("type").equals("image")) {
@@ -140,36 +134,35 @@ public class MediaServlet extends HttpServlet {
         } else {
             response.getWriter().write(makeJSON(null, "pathfail"));
         }
-	    
+
         path = "/res/" + post + "/" + type + "/";
         File dir = new File(realPath + post + "/" + type);
-	    
-        if(!dir.isDirectory()) {
+
+        if (!dir.isDirectory()) {
             dir.mkdirs();
-        } 
-	    
-	    try {
-	        mul = new MultipartRequest(request, dir.getPath(), FileMaxSize, "UTF-8", 
-	                new DefaultFileRenamePolicy());
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	        response.getWriter().write(makeJSON(e.toString(),"uploadfail"));
-	        return;
-	    }
-	    
-	    fileList = dir.listFiles();
-	    
-	    for (File temp : fileList) {
-	        String tempPath = path + temp.getName();
-	        tempMedia.setPath(tempPath);
-	        createMedia = MediaDao.createMedia(tempPost, tempMedia);
-	        
-	        if(!createMedia.didSucceed()) {
-	            response.getWriter().write(makeJSON(createMedia.getException().getMessage(), "dbfail"));
-	            return;
-	        }
-	    }
-	    
-	    response.getWriter().write(makeJSON(null, "success"));
-	}
+        }
+
+        try {
+            mul = new MultipartRequest(request, dir.getPath(), FileMaxSize, "UTF-8", new DefaultFileRenamePolicy());
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.getWriter().write(makeJSON(e.toString(), "uploadfail"));
+            return;
+        }
+
+        fileList = dir.listFiles();
+
+        for (File temp : fileList) {
+            String tempPath = path + temp.getName();
+            tempMedia.setPath(tempPath);
+            createMedia = MediaDao.createMedia(tempPost, tempMedia);
+
+            if (!createMedia.didSucceed()) {
+                response.getWriter().write(makeJSON(createMedia.getException().getMessage(), "dbfail"));
+                return;
+            }
+        }
+
+        response.getWriter().write(makeJSON(null, "success"));
+    }
 }
--- wup-app/src/wup/servlet/PostDeleteServlet.java
+++ wup-app/src/wup/servlet/PostDeleteServlet.java
@@ -1,6 +1,7 @@
 package wup.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,37 +19,39 @@ import wup.data.access.PostDao;
 
 @WebServlet("/postdelete/*")
 public class PostDeleteServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    //json 변환 메소드
+    private static final long serialVersionUID = 1L;
+
+    // json 변환 메소드
     private String makeJSON(Object obj, String result) {
         Gson gson = new Gson();
         JsonObject jobj = new JsonObject();
-        
+
         String objson = gson.toJson(obj);
         String json;
-        
+
         jobj.addProperty("data", objson);
         jobj.addProperty("result", result);
-        
+
         json = gson.toJson(jobj);
-        
+
         return json;
     }
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    response.setContentType("application/json");
-	    
-	    MariaDbDaoFactory daoFactory = new DaoFactory();
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+
+        MariaDbDaoFactory daoFactory = new DaoFactory();
         PostDao PostDao = (PostDao) daoFactory.getDao(Post.class);
-        
-        DaoResult <Boolean> deletePost = PostDao.deletePost(Integer.parseInt(request.getParameter("id")));
-        
-        if(deletePost.didSucceed()) {
+
+        DaoResult<Boolean> deletePost = PostDao.deletePost(Integer.parseInt(request.getParameter("id")));
+
+        if (deletePost.didSucceed()) {
             response.getWriter().write(makeJSON(null, "success"));
         } else {
             response.getWriter().write(makeJSON(deletePost.getException().getMessage(), "fail"));
         }
-	}
+    }
 
 }

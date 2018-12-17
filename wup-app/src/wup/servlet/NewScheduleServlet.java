92a281cd149861d833c961eb5f896c6101a2ef1d
Eunbin Jeong <dmsqls7353@gmail.com>
2018-12-17 21:59:54 +0900

    새로운 일정을 추가하는 서블릿 메서드 구현

--- wup-app/src/wup/servlet/NewScheduleServlet.java
+++ wup-app/src/wup/servlet/NewScheduleServlet.java
@@ -1,6 +1,10 @@
 package wup.servlet;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.GregorianCalendar;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,7 +12,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import wup.data.Planner;
+import wup.data.Schedule;
 import wup.data.User;
+import wup.data.access.DaoFactory;
+import wup.data.access.DaoResult;
+import wup.data.access.MariaDbDaoFactory;
+import wup.data.access.ScheduleDao;
 
 /**
  * Servlet implementation class NewScheduleServlet
@@ -16,6 +26,19 @@ import wup.data.User;
 @WebServlet("/newSchedule")
 public class NewScheduleServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    private static final String[] stringFields;
+    private static final String[] numberFields;
+
+    static {
+        stringFields = new String[] {
+                "title", "location", "allday", "description"
+        };
+
+        numberFields = new String[] {
+                "plannerId", "start_year", "start_month", "start_date", "start_hour", "start_minute", "end_year",
+                "end_month", "end_date", "end_hour", "end_minute"
+        };
+    }
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
@@ -58,6 +81,46 @@ public class NewScheduleServlet extends HttpServlet {
             response.sendRedirect(request.getContextPath() + "/login.jsp");
 
             return;
+        }
+
+        Map<String, String> strings = Arrays.stream(stringFields)
+                .collect(Collectors.toMap(name -> name, name -> ServletHelper.trimString(request.getParameter(name))));
+
+        Map<String, Integer> numbers = Arrays.stream(numberFields)
+                .collect(Collectors.toMap(name -> name, name -> parseNumber(request.getParameter(name))));
+
+        Planner planner = new Planner();
+        Schedule schedule = new Schedule();
+
+        planner.setId(numbers.get("plannerId"));
+
+        schedule.setTitle(strings.get("title"));
+        schedule.setLocation(strings.get("location"));
+        schedule.setStartsAt(new GregorianCalendar(numbers.get("start_year"), numbers.get("start_month") - 1,
+                numbers.get("start_date"), numbers.get("start_hour"), numbers.get("start_minute")).getTime());
+        schedule.setEndsAt(new GregorianCalendar(numbers.get("end_year"), numbers.get("end_month") - 1,
+                numbers.get("end_date"), numbers.get("end_hour"), numbers.get("end_minute")).getTime());
+        schedule.setAllDay(strings.get("allday").equals("on"));
+        schedule.setDescription(strings.get("description"));
+
+        MariaDbDaoFactory daoFactory = new DaoFactory();
+        ScheduleDao scheduleDao = (ScheduleDao) daoFactory.getDao(Schedule.class);
+        DaoResult<Schedule> createScheduleResult = scheduleDao.createSchedule(planner, schedule);
+
+        if (!createScheduleResult.didSucceed()) {
+            throw new ServletException(createScheduleResult.getException());
+        }
+
+        Schedule createdSchedule = createScheduleResult.getData();
+
+        response.sendRedirect(request.getContextPath() + "/schedule/" + createdSchedule.getId());
+    }
+
+    private Integer parseNumber(String value) {
+        try {
+            return Integer.parseInt(ServletHelper.trimString(value));
+        } catch (NumberFormatException e) {
+            return null;
         }
     }
 
